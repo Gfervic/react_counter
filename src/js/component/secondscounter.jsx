64592574a552eaae8d8fab771ebf9dacb83fd138
@betagon <gon.fervic@gmail.com>
2022-01-17 17:02:07 +0000

    finished the exercise

---
+++ src/js/component/secondscounter.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const SecondsCounter = (props) => {
+	return (
+		<div className="counterDiv">
+			<div className="calendar" id="calender">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="six">{props.digitSix % 10}</div>
+			<div className="five">{props.digitFive % 10}</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+};
+
+SecondsCounter.propTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number,
+};
+
+export default SecondsCounter;
